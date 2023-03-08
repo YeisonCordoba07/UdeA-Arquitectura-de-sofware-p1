38ec76f93bfd1e0d7c2dee35305c71da056fb51a
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-03-07 22:23:54 -0500

    completa el proyecto hasta el captcha

--- clientelab2023-web/src/main/java/com/udea/controller/CustomerMBean.java
+++ clientelab2023-web/src/main/java/com/udea/controller/CustomerMBean.java
@@ -8,7 +8,9 @@ package com.udea.controller;
 import com.udea.NewSessionBean;
 import com.udea.ejb.DiscountCodeManagerLocal;
 import com.udea.ejb.ZipCodeManagerLocal;
+import com.udea.modelo.Customer;
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.EJB;
 
 /**
@@ -30,15 +32,55 @@ public class CustomerMBean implements Serializable{
     private com.udea.ejb.CustomerManagerLocal customerManager;
     
 
-    //se agrega el codigo de la aplicacion
-    
-    /**
-     * Creates a new instance of CustomerMBean
-     */
+    private Customer customer; //para actualizar el formulario
+    private List<Customer> customers;   //para visualizar en la tabla dinamica
     
     
     
     public CustomerMBean() {
     }
+    
+    
+    //Metodo para retornar una lista de clientes para mostarr en la Datatable JSF
+    public List<Customer> getCustomers(){
+        if((customers == null) || (customers.isEmpty())){
+            refresh();
+        }
+        return customers;
+    }
+    
+    //Retorne el detalle de cada cliente en el formulario
+    public Customer getDetails(){
+        return customer;
+    }
+    
+    //ACTION HANDLER
+    //Es llamado cuando en una fila de la tabla se haga click en el ID
+    public String showDetails(Customer customer){
+        this.customer = customer;
+        return "DETAILS";
+    }
+    
+    
+    //ACTION HANDLER
+    //Actuliza el modelo Customer en el BD
+    //Se llama cuando se presiona el boton update del formulario
+    public String update(){
+        customer = customerManager.update(customer);
+        return "SAVED";
+    }
+    
+    
+    //ACTION HANDLER - retorna la lista de clientes
+    public String list(){
+        return "LIST";
+    }
+    
+    
+    public void refresh(){
+        customers = customerManager.getAllCustomers();
+    }
+    
+    
     
 }
